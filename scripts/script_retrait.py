8ac9b8cc79b880d1763973cfdf31301bdd6ffd6d
SiWaa13 <162142960+SiWaa13@users.noreply.github.com>
2025-04-16 15:13:43 +0200

    Update and rename tt to script_retrait.py

---
+++ scripts/script_retrait.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python3
+# Script fictif de retrait d’un utilisateur biométrique
+
+def retirer_utilisateur(uid):
+    print(f"Suppression de l’empreinte de l’utilisateur LDAP : {uid}")
+    print("Déconnexion des droits biométriques…")
+    print("Mise à jour du registre...")
+    print("Logs envoyés au SIEM.")
+
+# Exemple d’utilisation
+retirer_utilisateur("jdupont")
